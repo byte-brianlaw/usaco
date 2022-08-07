dabbea62dbcf4d3b85a76b1f2424122726a64f21
Brian Law <byte.brianlaw@gmail.com>
2022-08-07 00:51:33 -0700

    Modify `2016 December Bronze 1`
    - Modify `2016 December Bronze 1`

--- src/2016/december/bronze/1.cpp
+++ src/2016/december/bronze/1.cpp
@@ -16,9 +16,12 @@ auto solve() {
 
     auto pasture_1 = Pasture();
     auto pasture_2 = Pasture();
+    const auto inputPasture = [&](Pasture& pasture) {
+        fin >> pasture[0][0] >> pasture[0][1] >> pasture[1][0] >> pasture[1][1];
+    };
 
-    fin >> pasture_1[0][0] >> pasture_1[0][1] >> pasture_1[1][0] >> pasture_1[1][1] >>
-    pasture_2[0][0] >> pasture_2[0][1] >> pasture_2[1][0] >> pasture_2[1][1];
+    inputPasture(pasture_1);
+    inputPasture(pasture_2);
 
     const auto side = max(
         max(pasture_1[1][0], pasture_2[1][0]) - min(pasture_1[0][0], pasture_2[0][0]),
