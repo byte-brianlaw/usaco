0fb33d29f6aac036fa8a6d58917dd8a267e3592c
Brian Law <byte.brianlaw@gmail.com>
2022-11-27 15:09:21 -0800

    Modify `2017 December Silver 2`
    - Modify `2017 December Silver 2`

--- src/2017/december/silver/2.cpp
+++ src/2017/december/silver/2.cpp
@@ -1,26 +1,21 @@
 #include <algorithm>
+#include <array>
 #include <fstream>
 #include <set>
 #include <unordered_map>
 #include <vector>
 
+using std::array;
 using std::ifstream;
 using std::multiset;
 using std::ofstream;
 using std::sort;
 using std::unordered_map;
 using std::vector;
+using Entry = array<int, 3>;
 
 auto fin = ifstream("measurement.in");
 auto fout = ofstream("measurement.out");
-
-struct Entry {
-
-    int cow = 0;
-    int day = 0;
-    int change = 0;
-
-};
 
 auto solve() {
 
@@ -32,13 +27,13 @@ auto solve() {
     auto entries = vector<Entry>(n);
 
     for (auto& x : entries) {
-        fin >> x.day >> x.cow >> x.change;
+        fin >> x[2] >> x[0] >> x[1];
     }
 
     sort(
         entries.begin(), entries.end(),
         [](const Entry& entry_1, const Entry& entry_2) {
-            return entry_1.day < entry_2.day;
+            return entry_1[2] < entry_2[2];
         }
     );
 
@@ -47,19 +42,19 @@ auto solve() {
     auto outputs = multiset<int>({g});
 
     for (const auto& x : entries) {
-        if (cows.count(x.cow) == 0) {
-            cows[x.cow] = g;
+        if (cows.count(x[0]) == 0) {
+            cows[x[0]] = g;
             outputs.insert(g);
         }
-        auto& output = cows[x.cow];
+        auto& output = cows[x[0]];
         const auto shown = output == *outputs.rbegin();
         outputs.erase(outputs.find(output));
         const auto maximum = *outputs.rbegin();
         adjustments += (
-            output == maximum || (shown && output + x.change <= maximum) ||
-            (!shown && output + x.change >= maximum)
+            output == maximum || (shown && output + x[1] <= maximum) ||
+            (!shown && output + x[1] >= maximum)
         );
-        output += x.change;
+        output += x[1];
         outputs.insert(output);
     }
 
